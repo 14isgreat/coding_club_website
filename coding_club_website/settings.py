7dde470b9bf2f47160cb4d527202ee083f661e3c
Nicolas Pantelidis <nicolas@nicolass-mbp.speedport.ip>
2025-05-30 10:28:38 +0200

    add intro video

--- coding_club_website/settings.py
+++ coding_club_website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*jm6__l(w*fv(bw22qg1c+60nn_m0uusc(@dn%vr9ja%vj2p(r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codingclub.pythonanywhere.com']
+ALLOWED_HOSTS = ['codingclub.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
